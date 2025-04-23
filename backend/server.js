7d13c1e0ee2eafa0f92419d1b2e7d75beb7e359a
sarath <amsarathprakash07@gmail.com>
2025-04-23 20:06:09 +0530

    Final CORS fix

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,25 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: true, // Reflects request origin
-    credentials: true
-  }));
-
-  app.options('*', cors({
-    origin: true,
-    credentials: true
-  }));
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://fix-friend.vercel.app',
+  ];
+  
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+  
   
   
 app.use(express.json());
